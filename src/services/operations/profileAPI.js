6e499b2896368cfe1d2d1a159af500adb98e1e11
mayank <mayanksharma151203@gmail.com>
2025-08-17 15:29:03 +0530

    update commit

--- src/services/operations/profileAPI.js
+++ src/services/operations/profileAPI.js
@@ -5,7 +5,7 @@ import { apiConnector } from "../apiconnector"
 import { profileEndpoints } from "../apis"
 import { logout } from "./authAPI"
 
-const { GET_USER_DETAILS_API, GET_USER_ENROLLED_COURSES_API } = profileEndpoints
+const { GET_USER_DETAILS_API, GET_USER_ENROLLED_COURSES_API ,  GET_INSTRUCTOR_DATA_API} = profileEndpoints
 
 export function getUserDetails(token, navigate) {
   return async (dispatch) => {
