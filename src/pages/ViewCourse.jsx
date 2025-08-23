3949d7f856b7e43ebb16bbee6567bc0c5c145483
mayank <mayanksharma151203@gmail.com>
2025-08-23 16:28:29 +0530

    update

--- src/pages/ViewCourse.jsx
+++ src/pages/ViewCourse.jsx
@@ -1,90 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useParams, useNavigate, useLocation } from 'react-router-dom';
-import { getFullDetailsOfCourse } from '../services/operations/courseDetailsAPI';
-import { setCompletedLectures, setCourseSectionData, setEntireCourseData, setTotalNoOfLectures } from '../slices/viewCourseSlice';
-import VideoDetailsSidebar from '../components/core/ViewCourse/VideoDetailsSidebar';
-import CourseReviewModal from '../components/core/ViewCourse/CourseReviewModal';
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Outlet, useParams } from "react-router-dom"
 
-const ViewCourse = () => {
-    console.log("viewcourse component mounted")
+import CourseReviewModal from "../components/core/ViewCourse/CourseReviewModal"
+import VideoDetailsSidebar from "../components/core/ViewCourse/VideoDetailsSidebar"
+import { getFullDetailsOfCourse } from "../services/operations/courseDetailsAPI"
+import {
+  setCompletedLectures,
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+} from "../slices/viewCourseSlice"
 
-    const [reviewModal, setReviewModal] = useState(false);
-    const {courseId,sectionId,subSectionId} = useParams();
-    const {token} = useSelector((state)=>state.auth);
-    const {courseSectionData,entireCourseData} =useSelector((state)=>state.viewCourse);
-    const dispatch = useDispatch();
-    const navigate=useNavigate();
-    const location=useLocation();
+export default function ViewCourse() {
+  const { courseId } = useParams()
+  const { token } = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const [reviewModal, setReviewModal] = useState(false)
 
-      // Derived state for checking if params are missing
-  // const isLoadingParams = !sectionId || !subSectionId;
+  useEffect(() => {
+    ;(async () => {
+      const courseData = await getFullDetailsOfCourse(courseId, token)
+      // console.log("Course Data here... ", courseData.courseDetails)
+      dispatch(setCourseSectionData(courseData.courseDetails.courseContent))
+      dispatch(setEntireCourseData(courseData.courseDetails))
+      dispatch(setCompletedLectures(courseData.completedVideos))
+      let lectures = 0
+      courseData?.courseDetails?.courseContent?.forEach((sec) => {
+        lectures += sec.subSection.length
+      })
+      dispatch(setTotalNoOfLectures(lectures))
+    })()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-
-    useEffect(()=> {
-        const setCourseSpecificDetails = async() => {
-         try{
-                 const courseData = await getFullDetailsOfCourse(courseId, token);
-
-            console.log("API raw courseData:", courseData);
-          console.log("courseDetails:", courseData?.courseDetails);
-           console.log("courseContent:", courseData?.courseDetails?.courseContent);
-              dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
-              dispatch(setEntireCourseData(courseData.courseDetails));
-              dispatch(setCompletedLectures(courseData.completedVideos));
-              let lectures = 0;
-              courseData.courseDetails.courseContent.forEach((sec) => {
-                lectures += sec.subSection.length
-              })  ;
-              dispatch(setTotalNoOfLectures(lectures));
-        }
-        catch(error){
-          console.error("Failed to fetch course details:",error);
-        }
-         };
-        
-        setCourseSpecificDetails();
-    },[]);
-
-    useEffect(() => {
-    if (
-      courseSectionData.length > 0 &&
-      (!sectionId || !subSectionId)
-    ) {
-     
-      const firstSectionId = courseSectionData[0]?._id;
-      const firstSubSectionId = courseSectionData[0]?.subSection[0]?._id;
-     //  console.log(firstSectionId,firstSubSectionId,">>?????????????????????????????????")
-      navigate(
-        `/view-course/${courseId}/section/${firstSectionId}/sub-section/${firstSubSectionId}`,
-         { replace: true }
-      );
-    }
-  }, [courseSectionData, courseId, sectionId, subSectionId, navigate]);
-
-// console.log(courseSectionData,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
   return (
     <>
-        <div>
-            <VideoDetailsSidebar setReviewModal={setReviewModal} />
-              <div className="flex-1 overflow-y-auto">
-        {(!sectionId || !subSectionId) ? (
-          <div className="flex items-center justify-center h-full">
-            <p>Loading video...</p>
+      <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+        <VideoDetailsSidebar setReviewModal={setReviewModal} />
+        <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+          <div className="mx-6">
+            <Outlet />
           </div>
-        ) : (
-          <Outlet />
-        )}
-      </div>
-            
-            
-            {reviewModal && (<CourseReviewModal setReviewModal={setReviewModal} />)}
-
-           
         </div>
-        
+      </div>
+      {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
     </>
   )
 }
-
-export default ViewCourse
